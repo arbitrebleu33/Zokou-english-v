cf078403cbe85521fdea6b2082354f62b5269311
arbitrebleu33 <martinewambo2@gmail.com>
2024-08-29 20:45:04 +0200

    Update maes.js

--- commandes/maes.js
+++ commandes/maes.js
@@ -39,3 +39,33 @@ zokou(
         }
     }
 );
+zokou(
+    {
+        nomCom: 'banque',
+        categorie: 'Maestria rpverse'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = '  https://telegra.ph/file/9a1c19808b8b4e6bcf636.jpg';
+            const msg = `  🧭𝐂𝐎𝐌𝐏𝐓𝐄 𝐁𝐀𝐍𝐂𝐀𝐈𝐑𝐄🧭
+▬▬▭▬▬▭▬▭▬▬▭▬▭▬▬▭
+
+ROLISTE 👤:
+
+ACHAT :
+
+💰 SOLDES :💰
+
+NEW SOLDE :00
+
+DATE DE TRANSACTION📆:
+░░░░░░░░░░░░░░░░░░░
+  🌐MAESTRIA RP VERSE🌐
+░░░░░░░░░░░░░░░░░░░ `;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
